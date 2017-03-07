6a4d55210412b2bb482e4d5e935607589bf8dac8
Jeremias Pereira <jeremiastpereira@gmail.com>
2017-03-07 20:32:10 -0300

    Day 2

--- src/scripts/day-02.js
+++ src/scripts/day-02.js
@@ -1,1 +1,24 @@
-console.log('day two now');
+var day02 = function() {
+  const secondHand = document.querySelector('.hand-second');
+  const minsHand = document.querySelector('.hand-min');
+  const hourHand = document.querySelector('.hand-hour');
+
+  function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+    
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+    console.log(now);
+  }
+
+  setInterval(setDate, 1000);
+}
